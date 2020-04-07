c63216ab7f0b466f3dd0136dfd07b1f6b60b8d6f
rocean <rocean74@gmail.com>
2020-04-07 22:58:56 +0300

    Processes form

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1>Create Gantt Charts for CPU Processes</h1>
+
+    <div class="row">
+      <processes :processes="processes" class="mx-auto" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import processes from "@/components/Processes"
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { processes },
+
+  data() {
+    return {
+      processes: [
+        {
+          id: 0,
+          name: 'P1',
+          timeEnd: 10
+        },
+        {
+          id: 1,
+          name: 'P2',
+          timeEnd: 20
+        },
+        {
+          id: 2,
+          name: 'P3',
+          timeEnd: 30
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Processes.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div v-for="process in processes" :key="process.id">
+            <div class="form-group row">
+                <div class="col-lg-8">
+                    <input id="name" type="text" class="form-control"
+                           v-model="process.name"
+                           placeholder="Process Name" required maxlength="20">
+                </div>
+                <div class="col-lg-4">
+                    <input id="timeEnd" type="number" class="form-control"
+                           v-model="process.timeEnd"
+                           placeholder="Time End" required min="1" max="99">
+                </div>
+            </div>
+        </div>
+
+        <button class="btn btn-info" name="add" @click="addProcess">Add process</button>
+    </div>
+</template>
+
+<script>
+    export default {
+    	data() {
+    		return {
+
+            }
+        },
+
+    	props: {
+    		processes: {
+				required: true,
+				type: Array
+            }
+        },
+
+        methods: {
+    		addProcess()
+            {
+            	this.processes.push({
+                    id: null,
+                    name: '',
+                    timeEnd: null
+                })
+            }
+        }
+    }
+</script>
