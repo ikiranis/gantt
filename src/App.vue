54d4c646b3d308d0d922df210d9cf1aa0c9025cf
rocean <rocean74@gmail.com>
2020-04-08 03:53:26 +0300

    Styling

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       <gantt :processes="processes" class="mx-auto" />
     </div>
 
-    <div class="row mt-5">
+    <div class="row mt-5 mb-5">
       <div class="mx-auto">
         Created by <strong>Yiannis Kiranis</strong> (2020)
         <br><a href="https://apps4net.eu">apps4net.eu</a> |
